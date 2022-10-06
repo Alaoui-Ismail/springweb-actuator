462db3b9d99a5590b47404115f6c8043d4b0e9d4
Ismail ALAOUI <ismail.alaouihanafi@orange.com>
2022-10-06 12:27:48 +0100

    initial commit

---
+++ src/main/java/com/demo/springwebactuator/config/DemoController.java
@@ -0,0 +1,17 @@
+package com.demo.springwebactuator.config;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class DemoController {
+
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hellopage";
+    }
+}
---
+++ src/main/java/com/demo/springwebactuator/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.demo.springwebactuator.config;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.demo.springwebactuator")
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@PropertySource(value = { "classpath:application.properties" })
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
