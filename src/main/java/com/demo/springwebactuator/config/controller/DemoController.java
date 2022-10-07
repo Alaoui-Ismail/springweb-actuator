ce9b03c8ee03a3da2a15b6bb8071e3546c68ad2a
Ismail ALAOUI <ismail.alaouihanafi@orange.com>
2022-10-07 09:26:54 +0100

    return Hello

--- src/main/java/com/demo/springwebactuator/config/controller/DemoController.java
+++ src/main/java/com/demo/springwebactuator/config/controller/DemoController.java
@@ -1,17 +1,17 @@
-package com.demo.springwebactuator.config;
-
-
+package com.demo.springwebactuator.config.controller;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api")
+@ComponentScan("com.demo.springwebactuator.config")
 public class DemoController {
 
 
     @GetMapping("/hello")
-    public String sayHello(){
-        return "Hellopage";
+    public String sayHello() {
+        return "hellopage";
     }
 }
